ec38fb2a568b9cff97c63368d234bec2738dbe09
Afif Hossain <66325537+hossainafif@users.noreply.github.com>
2020-08-30 09:04:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function change() {
+    document.getElementById('h1').innerHTML = "Hello SSB";
+    document.getElementById('bt').innerHTML = "Double Click Me";
+}
+
+function change2() {
+    document.getElementById('h1').innerHTML = "Hello world!";
+    document.getElementById('bt').innerHTML = "Click Me";
+}
